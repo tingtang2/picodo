e4179cbcdeb722da34fdfe9a9cf1eb2c6518cd8b
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-03-30 14:25:11 +0100

    flatten_dict: use '.'

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def flatten_dict(d, prefix=None):
   if isinstance(d, Mapping):
     out = {}
     for k, v in d.items():
-      nested_prefix = k if prefix is None else f'{prefix}/{k}'
+      nested_prefix = k if prefix is None else f'{prefix}.{k}'
       out |= flatten_dict(v, nested_prefix)
     return out
   else:
