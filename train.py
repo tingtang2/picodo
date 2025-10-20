04e2abc1f2b450c41f730824e4df92c5b345e817
Ting Chen <zc2666@columbia.edu>
2025-10-20 16:50:57 +0000

    add checkpointing and logging of LR

--- train.py
+++ train.py
@@ -11,6 +11,9 @@ from tqdm.auto import tqdm
 from omegaconf.dictconfig import DictConfig
 import data, utils
 import model as model_lib
+import orbax.checkpoint as ocp
+from orbax.checkpoint.checkpoint_managers import preservation_policy 
+import os
 
 
 @partial(jax.jit, static_argnames=('model_graphdef'))
@@ -66,7 +69,7 @@ def train_and_evaluate(c: DictConfig):
     }
     for k, v in n_params.items():
         print(f'{k}={v:_}')
-
+    
     # dataset
     if (c.num_tokens_train is None) and (c.tokens_params_ratio is not None):
         c.num_tokens_train = c.tokens_params_ratio * (n_params['n_param_nonembed'] + n_params['n_param_embed'])
@@ -79,12 +82,48 @@ def train_and_evaluate(c: DictConfig):
     tokens_per_opt_step = c.opt.batch_size * c.model.T
     lr_schedule = optax.schedules.warmup_cosine_decay_schedule(0, c.opt.peak_lr, warmup_steps, num_opt_steps)
     tx = optax.inject_hyperparams(optax.adamw)(lr_schedule, c.opt.b1, c.opt.b2, weight_decay=c.opt.weight_decay)
+    
     clip_by_global_norm = c.opt.clip_by_global_norm
     if clip_by_global_norm:
         tx = optax.chain(
             optax.clip_by_global_norm(clip_by_global_norm), tx)
+    
     optimizer = nnx.ModelAndOptimizer(model, tx)
     opt_graphdef, opt_state = nnx.split(optimizer)
+
+    # set up checkpointing
+    start_step = 0
+    ckpt_mngr = None
+    abstract_opt_state = jax.tree_util.tree_map(ocp.utils.to_shape_dtype_struct, opt_state)
+    if c.checkpoint.turn_on:
+        run_name = c.run_name if c.run_name else 'picodo_run'
+        ckpt_dir = os.path.join(c.checkpoint.workdir, run_name)
+        
+        mngr_options = ocp.CheckpointManagerOptions(
+            create=True,
+            preservation_policy=preservation_policy.LatestN(c.checkpoint.max_to_keep)
+        )
+        
+        ckpt_mngr = ocp.CheckpointManager(
+            ckpt_dir,
+            options=mngr_options
+        )
+        
+        print(f'Checking for existing checkpoints in: {ckpt_dir}')
+        latest_step = ckpt_mngr.latest_step()
+
+        if latest_step is not None:
+            print(f'Restoring checkpoint from step {latest_step} in {ckpt_dir}...')
+            
+            restored_data = ckpt_mngr.restore(latest_step, args=ocp.args.Composite(state=ocp.args.StandardRestore(abstract_opt_state),
+            training_metadata=ocp.args.JsonRestore(),))
+            opt_state = restored_data['state']
+            start_step = restored_data['training_metadata']['step']
+            print(f'Successfully restored checkpoint. Resuming from step {start_step}.')
+        else:
+            print('No checkpoint found. Starting from scratch.')
+
+    model = nnx.merge(model_graphdef, opt_state.model)
 
     # start wandb
     if jax.process_index() == 0:
@@ -94,8 +133,8 @@ def train_and_evaluate(c: DictConfig):
     # training loop
     train_loss_sum, train_loss_num = jnp.zeros([]), 0
 
-    pbar = range(num_opt_steps)
-    if jax.process_index() == 0: pbar = tqdm(pbar)
+    pbar = range(start_step, num_opt_steps)
+    if jax.process_index() == 0: pbar = tqdm(pbar, initial=start_step, total=num_opt_steps)
     for step in pbar:
 
         # training step
@@ -108,6 +147,7 @@ def train_and_evaluate(c: DictConfig):
             metrics = {}
             metrics['train_loss'] = train_loss_sum / train_loss_num
             metrics['train_tokens_seen'] = (step+1) * tokens_per_opt_step
+            metrics['lr'] = lr_schedule(step)
             if jax.process_index() == 0:
                 wandb.log(metrics, step)
                 pbar.set_postfix_str(f'loss={metrics["train_loss"]:.2f}')
@@ -120,11 +160,23 @@ def train_and_evaluate(c: DictConfig):
             metrics['train_tokens_seen'] = (step+1) * tokens_per_opt_step
             if jax.process_index() == 0:
                 wandb.log(metrics, step)
-        # if step % c.checkpoint_every_steps == 0:
-        #     _checkpoint()
+
+        if c.checkpoint.turn_on and step % c.checkpoint.checkpoint_every_steps == 0:
+            ckpt_mngr.save(step, args=ocp.args.Composite(state=ocp.args.StandardSave(opt_state), training_metadata=ocp.args.JsonSave({
+                'step': step + 1})))
 
     # eval at end of training
     eval_loss = eval_step(opt_state.model, model_graphdef, ds_valid)
     if jax.process_index() == 0:
         wandb.log({'eval_loss': eval_loss}, step)
         wandb.finish()
+
+    # final checkpoint
+    if c.checkpoint.turn_on:
+        ckpt_mngr.save(step, args=ocp.args.Composite(state=ocp.args.StandardSave(opt_state), training_metadata=ocp.args.JsonSave({
+            'step': step + 1})))
+        
+        ckpt_mngr.wait_until_finished() 
+        if jax.process.index() == 0:
+            print(f'Saved final checkpoint at step {num_opt_steps} to {ckpt_mngr.directory}')
+        ckpt_mngr.close()
