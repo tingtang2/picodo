c549486adbec1739355ccfbc384c96e4989c749d
Ting Chen <zc2666@columbia.edu>
2026-07-11 16:46:08 +0000

    implement lazy data loading instead of eager

--- data.py
+++ data.py
@@ -6,7 +6,29 @@ from jax.sharding import PartitionSpec as P
 from jax.sharding import Mesh, NamedSharding
 
 
-def load_ds(key, mesh, ds_path, seq_len, batch_size, n_tokens_valid, n_tokens_train=None):
+class LazyBatchDataset:
+    def __init__(self, data, indices):
+        self.data = data
+        self.indices = np.asarray(indices, dtype=np.int64)
+        self.shape = (len(self.indices),) + tuple(data.shape[1:])
+        self.dtype = data.dtype
+        self.ndim = len(self.shape)
+        self.size = int(np.prod(self.shape))
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, index):
+        return np.asarray(self.data[self.indices[index]])
+
+    def __array__(self, dtype=None):
+        array = np.asarray(self[:])
+        if dtype is not None:
+            array = array.astype(dtype, copy=False)
+        return array
+
+
+def _load_ds_eager(key, mesh, ds_path, seq_len, batch_size, n_tokens_valid, n_tokens_train=None):
 
     # get dataset size
     print('getting dataset size...')
@@ -42,4 +64,41 @@ def load_ds(key, mesh, ds_path, seq_len, batch_size, n_tokens_valid, n_tokens_tr
     data_train = data[:n_batch_train]
     data_valid = data[n_batch_train:]
     
+    return data_train, data_valid
+
+
+def load_ds(key, mesh, ds_path, seq_len, batch_size, n_tokens_valid, n_tokens_train=None, lazy=True):
+
+    if not lazy:
+        return _load_ds_eager(key, mesh, ds_path, seq_len, batch_size, n_tokens_valid, n_tokens_train)
+
+    # get dataset size
+    print('getting dataset size...')
+    ds_path = os.path.expanduser(ds_path)
+    tokens = np.memmap(ds_path, dtype=np.uint16, mode='r')
+    n_tokens_dataset = len(tokens)
+
+    # if n_tokens_train is None, use full dataset
+    if n_tokens_train is not None: assert n_tokens_train + n_tokens_valid <= n_tokens_dataset
+    if n_tokens_train is None: n_tokens_train = n_tokens_dataset - n_tokens_valid
+
+    # get num. of train. and valid. batches
+    n_batch_train = n_tokens_train // (batch_size * seq_len)
+    n_batch_valid = n_tokens_valid // (batch_size * seq_len)
+    n_batch = n_batch_train + n_batch_valid
+
+    # memmap data
+    print('reading data lazily...')
+    data = np.memmap(ds_path, dtype=np.uint16, shape=[n_batch, batch_size, seq_len], mode='r')
+
+    # shuffle batch indices instead of shuffling the token array
+    print('shuffling batch indices...')
+    indices = jax.random.permutation(key, jnp.arange(n_batch, dtype=jnp.int32), axis=0)
+    indices = np.asarray(jax.device_get(indices), dtype=np.int64)
+
+    # split data
+    print('splitting indices...')
+    data_train = LazyBatchDataset(data, indices[:n_batch_train])
+    data_valid = LazyBatchDataset(data, indices[n_batch_train:])
+
     return data_train, data_valid
